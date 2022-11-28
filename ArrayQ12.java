44ae71344f0e0b0443192432667359e82b91f6cb
Deepa <deepakushwaha155@gmail.com>
2022-11-28 22:20:28 +0530

    12th commit

--- ArrayQ12.java
+++ ArrayQ12.java
@@ -1,6 +1,6 @@
-//Java Program to print the elements of an array
+//Java Program to print the elements of an array in reverse order
 import java.util.*;
-public class ArrayQ11 
+public class ArrayQ12 
 {
     public static void main(String[] args) 
     {
@@ -15,8 +15,8 @@ public class ArrayQ11
           arr[i]=sc.nextInt();
       }
       //loop to print the array element
-      System.out.println("Array elements are :");
-      for(int i=0;i<n;i++)
+      System.out.println("Array elements in reverse order are :");
+      for(int i=n-1;i>=0;i--)
       {
           System.out.print(arr[i]+" ");
       }
